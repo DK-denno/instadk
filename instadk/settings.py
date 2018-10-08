0762c2fe7bb192696d1ba28b21e34a7965c64279
dk-denno <dennisveer27@gmail.com>
2018-10-08 08:16:37 +0300

    deployment

--- instadk/settings.py
+++ instadk/settings.py
@@ -154,10 +154,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = config['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = config['EMAIL_HOST_PASSWORD']
-EMAIL_PORT = 587
+
 
 django_heroku.settings(locals())
